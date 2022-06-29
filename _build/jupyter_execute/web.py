e572649d9fd7e5d828ea55c54cb53a7a50fd2ee2
firyal190011 <firyal.indah.p@gmail.com>
2022-06-29 20:35:36 +0700

    revisi2

--- _build/jupyter_execute/web.py
+++ _build/jupyter_execute/web.py
@@ -1,15 +1,119 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # LSA
+# # Latent Semantic Analysis (LSA)
+
+# Latent Semantic Analysis (LSA) merupakan sebuah metode yang memanfaatkan model statistik matematis untuk menganalisa struktur semantik suatu teks. LSA bisa digunakan untuk menilai esai dengan mengkonversikan esai menjadi matriks-matriks yang diberi nilai pada masing-masing term untuk dicari kesamaan dengan term referensi
+
+# # Crawling data
+# Crawlling merupakan proses pengambilan data yang tersedia secara online untuk umum. Proses pengambilan data informasi pada halaman menggunakan URL (Uniform Resource Locator). URL ini akan menjadi acuan untuk mencari semua hyperlink yang ada pada website. Kemudian dilakukan indexing untuk mencari kata dalam dokumen pada setiap link yang ada. Lalu jika, menyertakan API (Application Programming Interface) dapat melakukan penambangan dataset yang lebih besar. Selain itu, dengan API kita dapat mengumpulkan data lebih spesifik sesuai dengan link URL yang ada tanpa harus mengetahui element HTML pada sebuah website.
+
+# ## Instalasi Scrapy
+# Untuk menginstall scrapy dapat menggnakan perintah pip di Command Prompt berikut:
+
+# In[1]:
+
+
+pip install scrapy
+
+
+# ## Membuat File Scrapy
+# Setelah selesai menginstall Scrapy, maka buat file scrapy baru dengan kode:
 
 # In[ ]:
 
 
+scrapy startproject namaproject
 
 
+# ## Menjalankan Spyder baru
+# Yang harus dilakukan, masuk ke dalam projectscrapy kemudian membuat spider baru dengan kode:
 
-# In[1]:
+# In[ ]:
+
+
+cd nama project
+
+
+# Setelah menjalankan spyder baru, tuliskan command yang isinya nama file dan alamat website yang akan diambil datanya agar lebih mudah saat akan membuat.
+
+# In[ ]:
+
+
+scrapy genspider example example.com
+
+
+# ## Menulis Program Scapper
+# Setelah itu, pada pembuatan spyder file yang tadi telah dibuat telah tersedia dengan nama file yang telah dibuat.
+# 
+# Pada file yang telah dibuat akan ada code untuk melakukan scraping yang dapat diubah sesuai keinginan
+
+# In[ ]:
+
+
+import scrapy
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'https://pta.trunojoyo.ac.id/welcome/detail/160411100077',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160211100071',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160531100046',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160521100004',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160541100008',
+            'https://pta.trunojoyo.ac.id/welcome/detail/150721100036',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160721100026',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160421100099',
+            'https://pta.trunojoyo.ac.id/welcome/detail/150721100026',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160521100008',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160421100169',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160211100041',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160311100017',
+            'https://pta.trunojoyo.ac.id/welcome/detail/130611100192',
+            'https://pta.trunojoyo.ac.id/welcome/detail/160521100043'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # print(response.url)
+        yield {
+            'judul': response.css('#content_journal > ul > li > div:nth-child(2) > a::text').extract(),
+            'penulis': response.css('#content_journal > ul > li > div:nth-child(2) > div:nth-child(2) > span::text').extract(),
+            'dosen_pembimbing_1': response.css('#content_journal > ul > li > div:nth-child(2) > div:nth-child(3) > span::text').extract(),
+            'dosen_pembimbing_2': response.css('#content_journal > ul > li > div:nth-child(2) > div:nth-child(4) > span::text').extract(),
+            'abstrak': response.css('#content_journal > ul > li > div:nth-child(4) > div:nth-child(2) > p::text').extract(),
+        }
+        # content_journal > ul > li:nth-child(1) > div:nth-child(1) > a
+        # content_journal > ul > li:nth-child(1) > div:nth-child(1) > a
+
+
+# ## Menjalankan File Spider
+# Pertama, masuk kedalam direktori spider dulu. setelah itu, jalankan spider dengan command
+
+# In[ ]:
+
+
+scrapy runspider namafile.py
+
+
+# ## Menyimpan data kedalam csv
+# Data yang telah diambil dari suatu web dapat disimpan dengan kode:
+
+# In[ ]:
+
+
+scrapy crawl namafile -O namafileyangdiinginkan.xlsx
+
+
+# # Preprocessing data
+# Preprocessing data adalah tahapan perbersihan data dari suatu kalimat atau kata, link, atau hal –hal yang tidak diperlukan untuk melakukan analisis sentiment. Dataset yang diperoleh dari proses crawling masih berbentuk kata atau kalimat yang tidak beraturan atau tidak terstruktur, sehingga membutuhkan proses yang disebut preprocessing data agar menghasilkan data yang bersih supaya mempermudah proses analisis. Lalu pada Preprocessing sendiri melibatkan validasi dan imputasi data. Tujuan dari validasi adalah untuk menilai tingkat kelengkapan dan akurasi data yang tersaring. Preprocessing data sangat penting karena kesalahan, redundan, missing value, dan data yang tidak konsisten menyebabkan berkurangnya akurasi hasil analisis. Jadi, sebelum mengolah data, kita harus memastikan bahwa data yang akan kita gunakan merupakan data "bersih". Ada beberapa cara yang bisa digunakan untuk membersihkan data, tergantung dari jenis masalah yang ada dalam kumpulan data. Adapun tahapan preprocessing data terdiri dari Cleaning data, Case Folding, Tokenizing, Stopword, dan Stemming.
+
+# ## Installasi library
+# lakukan installasi library yang diperlukan, jika telah terinstall maka langsung panggil saja 
+
+# In[ ]:
 
 
 get_ipython().system('pip install nltk')
@@ -17,7 +121,7 @@ get_ipython().system('pip install numpy')
 get_ipython().system('pip install nltk')
 
 
-# In[2]:
+# In[ ]:
 
 
 nltk.download('stopwords')
@@ -26,10 +130,12 @@ nltk.download('wordnet')
 nltk.download('omw-1.4')
 
 
-# #Preprocessing data
-# Preprocessing data adalah tahapan perbersihan data dari suatu kalimat atau kata, link, atau hal –hal yang tidak diperlukan untuk melakukan analisis sentiment. Dataset yang diperoleh dari proses crawling masih berbentuk kata atau kalimat yang tidak beraturan atau tidak terstruktur, sehingga membutuhkan proses yang disebut preprocessing data agar menghasilkan data yang bersih supaya mempermudah proses analisis. Lalu pada Preprocessing sendiri melibatkan validasi dan imputasi data. Tujuan dari validasi adalah untuk menilai tingkat kelengkapan dan akurasi data yang tersaring. Preprocessing data sangat penting karena kesalahan, redundan, missing value, dan data yang tidak konsisten menyebabkan berkurangnya akurasi hasil analisis. Jadi, sebelum mengolah data, kita harus memastikan bahwa data yang akan kita gunakan merupakan data "bersih". Ada beberapa cara yang bisa digunakan untuk membersihkan data, tergantung dari jenis masalah yang ada dalam kumpulan data. Adapun tahapan preprocessing data terdiri dari Cleaning data, Case Folding, Tokenizing, Stopword, dan Stemming.
+# 
 
-# In[ ]:
+# ## Import library yang digunakan
+# memanggil library-library yang digunakan
+
+# In[1]:
 
 
 # data visualisation and manipulation
@@ -66,26 +172,61 @@ from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 stop_words=set(nltk.corpus.stopwords.words('english'))
 
 
-# #Data set
+# ## Data set
+# berikut data yang digunakan pada program saat ini
 
-# In[ ]:
-
-
-df=pd.read_csv('jurnal.csv', usecols =['Abstrak_indo'])
+# In[2]:
 
 
-# In[ ]:
+df=pd.read_csv('jurnal.csv')
+
+
+# In[3]:
 
 
 df.head(10)
 
 
-# #Cleansing data
+# ## Drop data yang tidak digunakan
+
+# In[4]:
+
+
+df.drop(['Judul','Penulis', 'Dosbing_1', 'Dosbing_2'],axis=1, inplace=True)
+
+
+# In[5]:
+
+
+df.head(30)
+
+
+# ## Remove angka
+# pada proses ini penghapusan angka dan tanda-tanda baca yang ada pada data
+
+# In[6]:
+
+
+import string 
+import re
+
+def remove(text):
+    return re.sub(r"\d+","",text)
+
+
+# In[7]:
+
+
+df['Abstrak_indo_remove']=df['Abstrak_indo'].apply(remove)
+df.head()
+
+
+# ## Cleansing data
 # 
 #   Data cleansing atau yang disebut juga dengan data scrubbing merupakan suatu proses analisa mengenai kualitas dari data dengan mengubah. Bisa juga pengelola mengoreksi ataupun menghapus data tersebut. Data yang dibersihkan tersebut adalah data yang salah, rusak, tidak akurat, tidak lengkap dan salah format. Selain itu, Cleaning data adalah proses menghilangkan atau  menghapus  data  dari  suatu  kalimat  yang  memiliki unsur, seperti username (@username), hastag (#), URL, angka, kalimat yang redundance,emoticon (:@, :D ), dan tanda baca  yang tidak diperlukan pada saat proses analisis sentiment. Pada proses cleaningini pun juga terjadi proses perubahan data ke lowercase.
 # 
 
-# In[ ]:
+# In[8]:
 
 
 def clean_text(headline):
@@ -96,64 +237,107 @@ def clean_text(headline):
   return cleaned_text
 
 
-# In[ ]:
+# In[9]:
 
 
 # time taking
-df['Abstrak_indo_cleaned']=df['Abstrak_indo'].apply(clean_text)
+df['Abstrak_indo_cleaned']=df['Abstrak_indo_remove'].apply(clean_text)
 
 
-# In[ ]:
-
-
-df.head()
-
-
-# In[ ]:
-
-
-df.drop(['Abstrak_indo'],axis=1,inplace=True)
-
-
-# In[ ]:
+# In[10]:
 
 
 df.head()
 
 
-# In[ ]:
+# ## Menghapus data yang tidak dibutuhkan
+# hal ini bertujuan agar menggunakan data yang telah di cleansing data
+
+# In[11]:
+
+
+df.drop(['Abstrak_indo', 'Abstrak_indo_remove'],axis=1,inplace=True)
+
+
+# In[12]:
+
+
+df.head()
+
+
+# In[13]:
 
 
 df['Abstrak_indo_cleaned'][0]
 
 
-# In[ ]:
+# ## TF-IDF
+# TF — IDF adalah suatu metode algoritma yang berguna untuk menghitung bobot setiap kata yang umum digunakan. Metode ini juga terkenal efisien, mudah dan memiliki hasil yang akurat. Metode ini akan menghitung nilai Term Frequency (TF) dan Inverse Document Frequency (IDF) pada setiap token (kata) di setiap dokumen dalam korpus. Secara sederhana, metode TF-IDF digunakan untuk mengetahui berapa sering suatu kata muncul di dalam dokumen.
+
+# Term Frequency (TF) yaitu pembobotan/weight setiap kata (term) pada suatu dokumen berdasarkan jumlah kemunculannya dalam dokumen tersebut. Semakin besar jumlah kemunculan suatu kata dalam dokumen, maka semakin besar pula bobot yang diberikan (TF Tinggi) jadi nilai tertinggi merupakan  jumlah kemunculan/frekuensi.
+# 
+# Setelah menentukan Tf maka selanjutnya kita tentukan nilai IDF nya dapat dihitung dengan rumus :
+# $$
+# \begin{gathered}
+# Tf-Idf=t f_{i j} * i d f_{j} \\
+# Tf-Idf=t f_{i j} * \log \left(\frac{D}{d f}\right)
+# \end{gathered}
+# $$
+# 
+# Catatan
+# 
+# Df = jumlah dokumen yang didalamnya memuat term tertentu
+# 
+# D = Jumlah Dokumen yang di perbandingkan
+
+# In[14]:
 
 
 vect =TfidfVectorizer(stop_words=stop_words,max_features=1000) 
 # to play with. min_df,max_df,max_features etc...
 
 
-# In[ ]:
+# In[15]:
 
 
 vect_text=vect.fit_transform(df['Abstrak_indo_cleaned'])
 
 
-# In[ ]:
+# In[16]:
 
 
 print(vect_text.shape)
 print(vect_text)
 
 
-# In[ ]:
+# In[17]:
+
+
+vect.get_feature_names()
+
+
+# In[18]:
+
+
+vect_text.todense()
+
+
+# In[19]:
+
+
+df = pd.DataFrame(vect_text.todense().T, index=vect.get_feature_names(), columns=[f'{i+1}' for i in range (len(df))])
+df
+
+
+# # LSA
+
+# In[20]:
 
 
 idf=vect.idf_
 
 
-# In[ ]:
+# In[21]:
 
 
 dd=dict(zip(vect.get_feature_names(), idf))
@@ -164,7 +348,7 @@ print(dd['yang'])
 print(dd['wajah'])  # police is most common and forecast is least common among the news headlines.
 
 
-# In[ ]:
+# In[22]:
 
 
 from sklearn.decomposition import TruncatedSVD
@@ -173,14 +357,34 @@ lsa_model = TruncatedSVD(n_components=10, algorithm='randomized', n_iter=10, ran
 lsa_top=lsa_model.fit_transform(vect_text)
 
 
-# In[ ]:
+# Mirip dengan dokumen lain kita bisa melakukan ini. Namun perhatikan bahwa nilai tidak menambah 1 seperti di LSA itu bukan kemungkinan topik dalam dokumen.
+
+# In[23]:
 
 
 print(lsa_top)
 print(lsa_top.shape)  # (no_of_doc*no_of_topics)
 
 
-# In[ ]:
+# In[24]:
+
+
+l=lsa_top[0]
+print("Document 0 :")
+for i,topic in enumerate(l):
+  print("Topic ",i," : ",topic*100)
+
+
+# In[25]:
+
+
+print(lsa_model.components_.shape) # (no_of_topics*no_of_words)
+print(lsa_model.components_)
+
+
+# Sekarang bisa mendapatkan daftar kata-kata penting dari masing-masing untuk 10 topik seperti yang ditunjukkan. Untuk kesederhanaan di sini saya telah menunjukkan 10 kata untuk setiap topik
+
+# In[26]:
 
 
 # most important words for each topic
